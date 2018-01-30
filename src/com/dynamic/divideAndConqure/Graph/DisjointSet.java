5b8269e2412d17dd5dac71c0dab6265bfd32fb3b
Sumit Kumar Kesarwani <sumitku@zeta.tech>
2018-01-30 17:22:47 +0530

    Disjoint

--- src/com/dynamic/divideAndConqure/Graph/DisjointSet.java
+++ src/com/dynamic/divideAndConqure/Graph/DisjointSet.java
@@ -1,50 +1,112 @@
 package com.dynamic.divideAndConqure.Graph;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 //https://www.geeksforgeeks.org/union-find-algorithm-set-2-union-by-rank/
 public class DisjointSet {
-    public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner( new FileInputStream( new File( "dis.txt" )
+    ) );
+    int v = sc.nextInt();
+    int e = sc.nextInt();
+    GraphD g = new GraphD( v ,e);
+    g.makeGraph( sc );
+    System.out.println(g.isCycle());
+  }
 
-    }
-    public int find(int i,Subset[] set){
-        if(!(i >= 0 && i < set.length)){
-            return Integer.MIN_VALUE;
-        }
-        if(set[i].parent == i){
-            return i;
-        }
-        return find(set[i].parent,set);
-    }
 
-    public void union(int root1,int root2,Subset[] set){
-        if(find(root1,set) == find(root2,set)){
-            return;
-        }
-        int rank1 = set[root1].rank;
-        int rank2 = set[root2].rank;
-        if(rank1 < rank2){
-            set[root1].parent = root2;
-        }else if(rank1 > rank2){
-            set[root2].parent = root1;
-        }else{
-            set[root1].parent = root2;
-            set[root2].rank += 1;
-        }
+}
+
+class GraphD {
+  private int v;
+  private int e;
+  private GraphD[] g = null;
+  public Eadge[] eadge;
+  public GraphD(){
+
+  }
+  public GraphD(int v,int e) {
+    this.v = v;
+    this.e = e;
+    eadge = new Eadge[e];
+    for(int i = 0; i < e; i ++){
+      eadge[i] = new Eadge();
     }
-    public void createGraph(int v,int e){
-        GraphD g = new GraphD();
-        for(int i = 0; i < v ; i++){
-            Eadge eadge = new Eadge();
-            
-        }
+  }
+
+  public void makeGraph(Scanner sc) {
+    int i = 0;
+    while (sc.hasNext() && i < this.e) {
+      Eadge e = createGraph( sc.nextInt(), sc.nextInt() );
+      eadge[i] = e;
+      i++;
     }
+  }
+
+  public Eadge createGraph(int src, int dest) {
+    Eadge e = new Eadge();
+    e.src = src;
+    e.dest = dest;
+    return e;
+  }
+
+  public GraphD[] getGraph(){
+    return this.g;
+  }
+
+  public boolean isCycle(){
+    Subset[] subSet = new Subset[this.v];
+    for(int i = 0; i < this.v ; i++){
+      subSet[i] = new Subset();
+      subSet[i].parent = i;
+      subSet[i].rank = 0;
+    }
+    for(int i = 0; i < this.e ; i++){
+     int x1 =  find( eadge[i].src,subSet );
+     int x2 = find(eadge[i].dest,subSet);
+     //System.out.println(x1 +" : "+x2);
+     if(x1 == x2){
+       return true;
+     }
+     union( eadge[i].src,eadge[i].dest,subSet );
+    }
+    return false;
+  }
+  public int find(int i, Subset[] set) {
+    if (!(i >= 0 && i < set.length)) {
+      return Integer.MIN_VALUE;
+    }
+    if (set[i].parent == i) {
+      return i;
+    }
+    return find( set[i].parent, set );
+  }
+
+  public void union(int root1, int root2, Subset[] set) {
+    int findNode1 = find( root1,set );
+    int findNode2 = find( root2,set );
+    int rank1 = set[findNode1].rank;
+    int rank2 = set[findNode2].rank;
+    if (rank1 < rank2) {
+      set[findNode1].parent = findNode2;
+    } else if (rank1 > rank2) {
+      set[findNode2].parent = findNode1;
+    } else {
+      set[findNode1].parent = findNode2;
+      set[findNode2].rank += 1;
+    }
+  }
 }
-class GraphD{
-    public Eadge eadge;
+
+class Eadge {
+  public int src;
+  public int dest;
 }
-class Eadge{
-    public int src;
-    public int dest;
-}
-class Subset{
-    public int rank;
-    public int parent;
+
+class Subset {
+  public int rank;
+  public int parent;
 }
